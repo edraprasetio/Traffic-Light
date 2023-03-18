82ecb0c68f97dbafbf2357b7bc3d49840dfff190
Edra Prasetio <67980359+edraprasetio@users.noreply.github.com>
2023-03-18 11:58:50 -0700

    Update main.c

--- STM32_workspace_9.1/Project 0 Code.zip_expanded/Project0_Code/src/main.c
+++ STM32_workspace_9.1/Project 0 Code.zip_expanded/Project0_Code/src/main.c
@@ -1,139 +1,3 @@
-/*
-    FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
-    All rights reserved
-
-    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
-    This file is part of the FreeRTOS distribution.
-
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
-    ***************************************************************************
-    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
-    >>!   distribute a combined work that includes FreeRTOS without being   !<<
-    >>!   obliged to provide the source code for proprietary components     !<<
-    >>!   outside of the FreeRTOS kernel.                                   !<<
-    ***************************************************************************
-
-    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
-    link: http://www.freertos.org/a00114.html
-
-    ***************************************************************************
-     *                                                                       *
-     *    FreeRTOS provides completely free yet professionally developed,    *
-     *    robust, strictly quality controlled, supported, and cross          *
-     *    platform software that is more than just the market leader, it     *
-     *    is the industry's de facto standard.                               *
-     *                                                                       *
-     *    Help yourself get started quickly while simultaneously helping     *
-     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
-     *    tutorial book, reference manual, or both:                          *
-     *    http://www.FreeRTOS.org/Documentation                              *
-     *                                                                       *
-    ***************************************************************************
-
-    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
-    the FAQ page "My application does not run, what could be wwrong?".  Have you
-    defined configASSERT()?
-
-    http://www.FreeRTOS.org/support - In return for receiving this top quality
-    embedded software for free we request you assist our global community by
-    participating in the support forum.
-
-    http://www.FreeRTOS.org/training - Investing in training allows your team to
-    be as productive as possible as early as possible.  Now you can receive
-    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
-    Ltd, and the world's leading authority on the world's leading RTOS.
-
-    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
-    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
-    compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
-    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
-    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
-    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
-    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
-    licenses offer ticketed support, indemnification and commercial middleware.
-
-    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
-    engineered and independently SIL3 certified version for use in safety and
-    mission critical applications that require provable dependability.
-
-    1 tab == 4 spaces!
-*/
-
-/*
-FreeRTOS is a market leading RTOS from Real Time Engineers Ltd. that supports
-31 architectures and receives 77500 downloads a year. It is professionally
-developed, strictly quality controlled, robust, supported, and free to use in
-commercial products without any requirement to expose your proprietary source
-code.
-
-This simple FreeRTOS demo does not make use of any IO ports, so will execute on
-any Cortex-M3 of Cortex-M4 hardware.  Look for TODO markers in the code for
-locations that may require tailoring to, for example, include a manufacturer
-specific header file.
-
-This is a starter project, so only a subset of the RTOS features are
-demonstrated.  Ample source comments are provided, along with web links to
-relevant pages on the http://www.FreeRTOS.org site.
-
-Here is a description of the project's functionality:
-
-The main() Function:
-main() creates the tasks and software timers described in this section, before
-starting the scheduler.
-
-The Queue Send Task:
-The queue send task is implemented by the prvQueueSendTask() function.
-The task uses the FreeRTOS vTaskDelayUntil() and xQueueSend() API functions to
-periodically send the number 100 on a queue.  The period is set to 200ms.  See
-the comments in the function for more details.
-http://www.freertos.org/vtaskdelayuntil.html
-http://www.freertos.org/a00117.html
-
-The Queue Receive Task:
-The queue receive task is implemented by the prvQueueReceiveTask() function.
-The task uses the FreeRTOS xQueueReceive() API function to receive values from
-a queue.  The values received are those sent by the queue send task.  The queue
-receive task increments the ulCountOfItemsReceivedOnQueue variable each time it
-receives the value 100.  Therefore, as values are sent to the queue every 200ms,
-the value of ulCountOfItemsReceivedOnQueue will increase by 5 every second.
-http://www.freertos.org/a00118.html
-
-An example software timer:
-A software timer is created with an auto reloading period of 1000ms.  The
-timer's callback function increments the ulCountOfTimerCallbackExecutions
-variable each time it is called.  Therefore the value of
-ulCountOfTimerCallbackExecutions will count seconds.
-http://www.freertos.org/RTOS-software-timer.html
-
-The FreeRTOS RTOS tick hook (or callback) function:
-The tick hook function executes in the context of the FreeRTOS tick interrupt.
-The function 'gives' a semaphore every 500th time it executes.  The semaphore
-is used to synchronise with the event semaphore task, which is described next.
-
-The event semaphore task:
-The event semaphore task uses the FreeRTOS xSemaphoreTake() API function to
-wait for the semaphore that is given by the RTOS tick hook function.  The task
-increments the ulCountOfReceivedSemaphores variable each time the semaphore is
-received.  As the semaphore is given every 500ms (assuming a tick frequency of
-1KHz), the value of ulCountOfReceivedSemaphores will increase by 2 each second.
-
-The idle hook (or callback) function:
-The idle hook function queries the amount of free FreeRTOS heap space available.
-See vApplicationIdleHook().
-
-The malloc failed and stack overflow hook (or callback) functions:
-These two hook functions are provided as examples, but do not contain any
-functionality.
-*/
-
 /* Standard includes. */
 #include <stdint.h>
 #include <stdio.h>
@@ -160,6 +24,10 @@ functionality.
 #define green_led	LED4
 #define red_led		LED5
 #define blue_led	LED6
+
+#define Task1_Period 500
+#define Task2_Period 500
+#define Task3_Period 500
 
 
 /*
@@ -199,13 +67,9 @@ typedef struct dd_task_list {
 
 void delete_dd_task(uint32_t task_id);
 
-
-
-xQueueHandle xQueue_handle = 0;
-
-
 xQueueHandle xQueue_Generator = 0;
-
+xQueueHandle xQueue_DDS = 0;
+xQueueHandle xQueue_Monitor = 0;
 
 xTimerHandle Task1_Release_Timer = 0;
 xTimerHandle Task2_Release_Timer = 0;
@@ -229,21 +93,20 @@ int main(void)
 
 	/* Create the queue used by the queue send and queue receive tasks.
 	http://www.freertos.org/a00116.html */
-	xQueue_handle = xQueueCreate( 	mainQUEUE_LENGTH,		/* The number of items the queue can hold. */
-							sizeof( uint16_t ) );	/* The size of each item the queue holds. */
-
 	xQueue_Generator = xQueueCreate( 100, sizeof( uint32_t));
+	xQueue_DDS = xQueueCreate( 100, sizeof( uint32_t));
+	xQueue_Monitor = xQueueCreate( 100, sizeof( uint32_t));
 
 	/* Add to the registry, for the benefit of kernel aware debugging. */
-	vQueueAddToRegistry( xQueue_handle, "MainQueue" );
-
 	vQueueAddToRegistry( xQueue_Generator, "Generator Queue");
+	vQueueAddToRegistry( xQueue_DDS, "DDS Queue");
+	vQueueAddToRegistry( xQueue_Monitor, "Monitor Queue");
 
 	xTaskCreate( DD_Task_Generator, "Generate", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
-	Task1_Release_Timer = xTimerCreate( "Task 1 timer", pdMS_TO_TICKS(500), pdTRUE, (void *) 0, Task1_Callback);
-	Task2_Release_Timer = xTimerCreate( "Task 2 timer", pdMS_TO_TICKS(500), pdTRUE, (void *) 0, Task2_Callback);
-	Task3_Release_Timer = xTimerCreate( "Task 3 timer", pdMS_TO_TICKS(500), pdTRUE, (void *) 0, Task3_Callback);
+	Task1_Release_Timer = xTimerCreate( "Task 1 timer", pdMS_TO_TICKS(Task1_Period), pdTRUE, (void *) 0, Task1_Callback);
+	Task2_Release_Timer = xTimerCreate( "Task 2 timer", pdMS_TO_TICKS(Task2_Period), pdTRUE, (void *) 0, Task2_Callback);
+	Task3_Release_Timer = xTimerCreate( "Task 3 timer", pdMS_TO_TICKS(Task3_Period), pdTRUE, (void *) 0, Task3_Callback);
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -256,22 +119,30 @@ int main(void)
 
 
 void DD_Task_Generator( void *pvParameters ){
-
-	TaskHandle_t Task1_Handle = NULL;
-	TaskHandle_t Task2_Handle = NULL;
-	TaskHandle_t Task3_Handle = NULL;
-
-	xTimerStart( Task1_Release_Timer, 500);
-	xTimerStart( Task2_Release_Timer, 500);
-	xTimerStart( Task3_Release_Timer, 500);
-
-	Task1_Callback( Task1_Release_Timer);
-	Task2_Callback( Task2_Release_Timer);
-	Task3_Callback( Task3_Release_Timer);
-
+	uint32_t i = 0;
+	uint32_t test_bench = 1;
+	uint32_t test_period;
 
 	while(1){
-		xQueueReceive( xQueue_Generator, )
+		if (test_bench == 1){
+			test_period = 1500;
+			create_dd_task(PERIODIC, 1, 95, 500 + i * test_period);
+			create_dd_task(PERIODIC, 2, 150, 500 + i * test_period);
+			create_dd_task(PERIODIC, 3, 250, 750 + i * test_period);
+			vTaskDelay(pdMS_TO_TICKS(500));
+
+			create_dd_task(PERIODIC, 1, 95, 1000 + i * test_period);
+			create_dd_task(PERIODIC, 2, 150, 1000 + i * test_period);
+			vTaskDelay(pdMS_TO_TICKS(250));
+
+			create_dd_task(PERIODIC, 3, 250, 1500 + i * test_period);
+			vTaskDelay(pdMS_TO_TICKS(250));
+
+			create_dd_task(PERIODIC, 1, 95, 1500 + i * test_period);
+			create_dd_task(PERIODIC, 2, 150, 1500 + i * test_period);
+			vTaskDelay(pdMS_TO_TICKS(500));
+
+		}
 	}
 }
 
@@ -287,26 +158,23 @@ static void Task3_Callback(xTimerHandle pxTimer){
 
 }
 
-void create_dd_task( TaskHandle_t t_handle, uint32_t type, uint32_t task_id, uint32_t absolute_deadline) {
+void create_dd_task(uint32_t type, uint32_t task_id, uint32_t execution_time, uint32_t absolute_deadline) {
 
 	struct dd_task new_dd_task;
+	printf("CREATE_DD_TASK: Creating task %u\n", task_id);
 
 	new_dd_task.type = type;
 	new_dd_task.task_id = task_id;
 	new_dd_task.absolute_deadline = absolute_deadline;
 
-	release_dd_task(&new_dd_task);
+	if( xQueueSend( xQueue_DDS, (void *) &new_dd_task, 1000)) {
+		vTaskDelay(0);
+	} else {
+		printf("CREATE_DD_TASK: Failed!\n");
+	}
 
 
 }
-
-void release_dd_task( struct dd_task *new_task){
-
-	if( !xQueueSend(xQueue_handle, &new_task, 100)) {
-			printf("Create DD Task failed.\n");
-		}
-}
-
 
 
 /*-----------------------------------------------------------*/
@@ -370,4 +238,3 @@ static void prvSetupHardware( void )
 	/* TODO: Setup the clocks, etc. here, if they were not configured before
 	main() was called. */
 }
-
